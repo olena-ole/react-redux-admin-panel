38a1e51c15b031ae3e7a07f90cae2810551f6c49
Lenna <pampam32121@gmail.com>
2023-04-27 18:56:07 +0300

    used middleware instead of enhancers

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,47 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-
-        return oldDispatch(action);
+// 1st argument can be (store) OR store destructured ({dispatch, getState})
+// 2nd argumant is dispatch, called next (because it is a chain of dispatch fns in future)
+const stringMiddleware = () => next => action => { 
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
 
-    return store;
-} 
+    return next(action);
+}
+
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             })
+//         }
+
+//         return oldDispatch(action);
+//     }
+
+//     return store;
+// } 
 
 const store = createStore(
     combineReducers({ heroes, filters }),
     compose(
-        enhancer,
+        applyMiddleware(stringMiddleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
+    // compose(
+    //     enhancer,
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // )
 );
 
 export default store;
